0d5e36d15ae54ab8ffebf88f430bfb611062ab0a
Stormchaser1987 <berkayertass@hotmail.com>
2018-08-22 17:53:56 +0300

    Update tss.py

--- tss.py
+++ tss.py
@@ -11,7 +11,7 @@ import math
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import seaborn as sns
-data = pd.read_csv("TSSdata.txt")  # Verimizi okuyalım
+data = pd.read_csv("TSSdata.txt")  # Batı Avrupa verimizi okuyalım.
 x = data['Yil'] # 
 y = data['Pressure']
 z=sum(data['Pressure']/71)
@@ -27,10 +27,10 @@ regr = LinearRegression()
 regr.fit(data[['Yil']], data[['Pressure']])
 
 
-df = pd.read_csv("C:/Users/user/Desktop/TSSData2.txt")  # Verimizi okuyalım
+df = pd.read_csv("C:/Users/user/Desktop/TSSData2.txt")  # Türkiye verimizi okuyalım.
 a = df['Yil']
 b = df['Pressure']
-c=sum(df['Pressure']/71) # TSS formülü
+c=sum(df['Pressure']/71) 
 print(df)
 
 print(a)
@@ -40,10 +40,10 @@ plt.xlabel('')
 plt.ylabel('')
 plt.title('Türkiyede Yıllara Göre 500 MB Basınç-Temmuz')
 
-T=((y-z)-(b-c))/50
+T=((y-z)-(b-c))/50 # TSS formülü
 plt.title('Türkiye Soğuk Salınımı-Temmuz')
-plt.scatter(x,T) #Grafiğe dökelim.
+plt.scatter(x,T) # Grafiğe dökelim.
 plt.plot(x,T)
 pd.set_option('display.max_rows', 71)
-print(T) #Yazdıralım
+print(T) # Yazdıralım
 
